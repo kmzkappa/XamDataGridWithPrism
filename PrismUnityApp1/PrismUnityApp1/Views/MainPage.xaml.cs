a74c2d79060fd18dda49576a8bca98639112d814
kmz_kappa <kmz.kappa@gmail.com>
2017-07-20 02:22:21 +0900

    プロジェクト ファイルを追加します。

---
+++ PrismUnityApp1/PrismUnityApp1/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Infragistics.Data;
+using Xamarin.Forms;
+
+namespace PrismUnityApp1.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            DataGrid1.ItemsSource = SampleSalesPerson.GenerateSalesData(100);
+            DataGrid1.AutoGenerateColumns = true;
+        }
+    }
+}
